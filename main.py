403211c6fa39c69631c663b16af7ccbf88b61fb6
userkaranb <userkaranb@gmail.com>
2016-08-21 20:43:51 -0500

    Philosophie Assignment
    Patch 3) Added a few tests to make sure they align with the examples.
    
    TODO:
    - Refactor methods into logical classes
    - Provide realtime feedback for passwords (?)
    - Add Unit tests for functions

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from copy import copy
 def GetPasswordStrength(password):
     newPassword = ReplaceLongestSubstringWithASingleCharacter(password)
     numberOfCharacterTypes = GetNumberOfCharacterTypes(newPassword)
-    return len(newPassword) * numberOfCharacterTypes
+    return DetermineStrengthOfPasswordBucket(len(newPassword) * numberOfCharacterTypes)
+
+def DetermineStrengthOfPasswordBucket(score):
+    if score <= 10:
+        return 'Unacceptable'
+    elif score > 10 and score < 50:
+        return 'Weak'
+    else:
+        return 'Strong'
 
 def GetLongestSubstring(password):
     counter = 0
@@ -44,3 +52,14 @@ def GetNumberOfCharacterTypes(password):
         else:
             types.update(['other'])
     return len(types)
+
+
+def RunTests():
+    firstTest = 'password1'
+    secondTest = 'goat m4n'
+    thirdTest = 's0_0per 5n4k3'
+    print 'Strength of Password {0} is {1}'.format(firstTest, GetPasswordStrength(firstTest))
+    print 'Strength of Password {0} is {1}'.format(secondTest, GetPasswordStrength(secondTest))
+    print 'Strength of Password {0} is {1}'.format(thirdTest, GetPasswordStrength(thirdTest))
+
+RunTests()
