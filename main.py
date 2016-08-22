1c3aa844365802f10f7cca090adb3c90c90f7714
userkaranb <userkaranb@gmail.com>
2016-08-21 21:40:05 -0500

    Philosophie Assignment
    Patch 1) Find english words/Replacement
    Patch 2) Add Scoring based on character types
    Patch 3) Run Simple tests based on doc
    Patch 4) Fix Bugs

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from copy import copy
 
 def GetPasswordStrength(password):
-    newPassword = ReplaceLongestSubstringWithASingleCharacter(password)
-    numberOfCharacterTypes = GetNumberOfCharacterTypes(newPassword)
-    return DetermineStrengthOfPasswordBucket(len(newPassword) * numberOfCharacterTypes)
+    newPassword = ReplaceWordsWithACharacter(password)
+    score = CalculateScore(newPassword)
+    return DetermineStrengthOfPasswordBucket(score)
+
+def CalculateScore(password):
+    numberOfCharacterTypes = GetNumberOfCharacterTypes(password)
+    return len(password) * numberOfCharacterTypes
 
 def DetermineStrengthOfPasswordBucket(score):
     if score <= 10:
@@ -13,32 +17,32 @@ def DetermineStrengthOfPasswordBucket(score):
     else:
         return 'Strong'
 
-def GetLongestSubstring(password):
+def ReplaceWordsWithACharacter(password):
     counter = 0
     currentStartIndex = 0
     currentEndIndex = 0
-    longestSubstringStartIndex = 0
-    longestSubstringEndIndex = 0
+    wordsToReplace = []
     while counter < len(password):
         if password[counter].isalpha():
             currentEndIndex = counter
         else:
-            if currentEndIndex - currentStartIndex > longestSubstringEndIndex - longestSubstringStartIndex:
-                longestSubstringStartIndex = currentStartIndex
-                longestSubstringEndIndex = currentEndIndex
+            if currentEndIndex > currentStartIndex:
+                wordsToReplace.insert(0, (currentStartIndex, currentEndIndex))
             currentStartIndex = counter + 1
             currentEndIndex = counter + 1
         counter += 1
-    if currentEndIndex - currentStartIndex > longestSubstringEndIndex - longestSubstringStartIndex:
-                longestSubstringStartIndex = currentStartIndex
-                longestSubstringEndIndex = currentEndIndex
-    return password[longestSubstringStartIndex:longestSubstringEndIndex+1]
+    if currentStartIndex != currentEndIndex:
+        wordsToReplace.insert(0, (currentStartIndex, currentEndIndex))
+    password = ReplaceAllInstancesOfWords(wordsToReplace, password)
+    return password
 
-def ReplaceLongestSubstringWithASingleCharacter(password):
-    longestSubstring = GetLongestSubstring(password)
-    passCopy = copy(password)
-    passCopy = passCopy.replace(longestSubstring, 'x')
-    return passCopy
+def ReplaceAllInstancesOfWords(wordsToReplaceIndices, password):
+    for startIndex, endIndex in wordsToReplaceIndices:
+        startIndex = max(0, startIndex)
+        endIndex = min(len(password) - 1, endIndex)
+        password = password[0:startIndex] + 'x' + password[endIndex + 1:]
+    return password
+
 
 def GetNumberOfCharacterTypes(password):
     types = set()
@@ -58,8 +62,15 @@ def RunTests():
     firstTest = 'password1'
     secondTest = 'goat m4n'
     thirdTest = 's0_0per 5n4k3'
-    print 'Strength of Password {0} is {1}'.format(firstTest, GetPasswordStrength(firstTest))
-    print 'Strength of Password {0} is {1}'.format(secondTest, GetPasswordStrength(secondTest))
-    print 'Strength of Password {0} is {1}'.format(thirdTest, GetPasswordStrength(thirdTest))
+    fourthTest = 'abcde fghijk lo1xbz!'
+
+    print 'Strength of Password {0} is {1} and Score is {2}'.format(firstTest, GetPasswordStrength(firstTest),
+     CalculateScore(ReplaceWordsWithACharacter(firstTest)))
+    print 'Strength of Password {0} is {1} and Score is {2}'.format(secondTest, GetPasswordStrength(secondTest),
+     CalculateScore(ReplaceWordsWithACharacter(secondTest)))
+    print 'Strength of Password {0} is {1} and Score is {2}'.format(thirdTest, GetPasswordStrength(thirdTest),
+     CalculateScore(ReplaceWordsWithACharacter(thirdTest)))
+    print 'Strength of Password {0} is {1} and Score is {2}'.format(fourthTest, GetPasswordStrength(fourthTest),
+     CalculateScore(ReplaceWordsWithACharacter(fourthTest)))
 
 RunTests()
