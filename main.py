80137f3bc410302fb2c65e9c93749427da7f4675
userkaranb <userkaranb@gmail.com>
2016-08-22 22:40:44 -0500

    Philosophie Assignment
    Patch 1) Find english words/Replacement
    Patch 2) Add Scoring based on character types
    Patch 3) Run Simple tests based on doc
    Patch 4) Fix Bugs
    Patch 5) Read in dictionary from separate text file
    Patch 6) Create Unit Tests
    Patch 7) Made Main.py a more user friendly client. Added README with
    directions

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import sys
 from copy import copy
 from PasswordScorer import PasswordScorer
 
-def LoadDictionary():
-  file = open('dictionary.txt', 'r')
+def LoadDictionary(inputFile):
+  file = open(inputFile, 'r')
   return [x.strip() for x in file.readlines()]
 
+def main(dictionary):
+    waitForQuit = False
+    scorer = PasswordScorer(dictionary)
+    while not waitForQuit:
+        password = raw_input('Enter a password here to receive a score. Type "quit" to exit: ').lower().replace("\"", '')
+        if password == 'quit':
+            waitForQuit = True
+        else:
+            scorer.DisplayPasswordScoreAndStrength(password)
 
-LoadDictionary()
+if __name__ == "__main__":
+    dictionary = None
+    if len(sys.argv[1:]) > 0:
+        dictionary = LoadDictionary(sys.argv[1:][0])
+    else:
+        dictionary = LoadDictionary('dictionary.txt')
+
+    main(dictionary)
